8ee60d922f3e981297469a63bfd591ba4cc18783
soutovp <fernandoandradesouto@hotmail.com>
2022-06-24 10:23:04 -0300

    Routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+import {useRouter} from 'next/router'
 function Home(){
-  return <h1>Home Page</h1>
+  const router = useRouter();
+
+  const handleCLick = () =>{
+    console.log('Placing your order');
+    router.push('/product');
+  }
+
+  return (
+      <div>
+        <h1>Home Page</h1>
+        <Link href="/blog">
+        <a>Blog</a>
+        </Link>
+        <Link href="/product">
+          <a>Product</a>
+        </Link>
+        <button onClick={handleCLick}>
+          Place Order
+        </button>
+      </div>
+    )
 }
 
 export default Home
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,32 @@
-function ProductList(){
+import Link from 'next/link'
+
+function ProductList({ProductId = 100}){
     return(
         <>
-            <h3>Product 1</h3>
-            <h3>Product 2</h3>
-            <h3>Product 3</h3>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+            <hr/>
+            <h3>
+                <Link href="/product/1">
+                    <a>Product 1</a>
+                </Link>
+            </h3>
+            <h3>
+                <Link href="/product/2">
+                    <a>Product 2</a>
+                </Link>
+            </h3>
+            <h3>
+                <Link href="/product/3" replace>
+                    <a>Product 3</a>
+                </Link>
+            </h3>
+            <h3>
+                <Link href={`/product/${ProductId}`}>
+                    <a>Product {ProductId}</a>
+                </Link>
+            </h3>
         </>
     )
 }
