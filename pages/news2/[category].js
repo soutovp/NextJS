edcd2c1ba2a14a81bc1d56943b6e6e00d0f16759
soutovp <fernandoandradesouto@hotmail.com>
2022-06-27 14:40:26 -0300

    Added news2

---
+++ pages/news2/[category].js
@@ -0,0 +1,31 @@
+export default function ArticleListByCategory({articles, category}){
+    return(
+        <>
+            <h1>Showing news for category <i>{category}</i></h1>
+            {
+                articles.map(article=>{
+                    return(
+                        <div ket={article.id}>
+                            <h2>{article.id} {article.title}</h2>
+                            <p>{article.description}</p>
+                            <hr />
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export async function getServerSideProps(context){
+    const {params} = context;
+    const { category } = params;
+    const response = await fetch(`http://localhost:4000/news?category=${category}`);
+    const data = await response.json();
+    return{
+        props:{
+            articles: data,
+            category,
+        },
+    }
+}
